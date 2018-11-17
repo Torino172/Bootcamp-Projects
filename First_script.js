1725953d3ea00160b80b94fc28452bad33993b9a
Ettore Marques <torino172@gmail.com>
2018-11-16 23:27:49 -0200

    Initial commit

---
+++ First_script.js
@@ -0,0 +1,6 @@
+//var firsName = prompt("What's your first name?");
+//var lastName = prompt("What's your last name?");
+var age = prompt("How old are you?");
+//console.log("your name is " + firsName + lastName);
+//console.log("you're " + age + " years old");
+console.log(365 * age)
